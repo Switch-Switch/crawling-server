237a38baae8d1697c18d467ed2639b35b6d93c26
Eui9179 <outdark97@gmail.com>
2024-07-16 09:06:36 +0900

    B Feature/add-transactional-read-only (#1)

--- src/main/java/com/rljj/switchswitchcrawling/domain/chiptype/ChipTypeService.java
+++ src/main/java/com/rljj/switchswitchcrawling/domain/chiptype/ChipTypeService.java
@@ -1,13 +1,14 @@
 package com.rljj.switchswitchcrawling.domain.chiptype;
 
 import com.rljj.switchswitchcrawling.domain.crawling.CrawledChip;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class ChipTypeService {
 
@@ -18,6 +19,7 @@ public class ChipTypeService {
         chipTypeRepository.saveAll(chips.stream().map(ChipType::from).toList());
     }
 
+    @Transactional
     public void save(CrawledChip chip) {
         chipTypeRepository.save(ChipType.from(chip));
     }
